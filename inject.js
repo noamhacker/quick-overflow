652156088a08d2836cf18e7026eedc89f8d92c29
Noam <13nah@queensu.ca>
2016-02-28 19:09:54 -0500

    clean up files. improve detail and formatting on error 'boxes', and make script catch errors from the beginning

--- inject.js
+++ inject.js
@@ -1,35 +1,52 @@
-console.log("injects now")
-console.trace()
-document.onload = function(){
-	window.onerror = function(error, url, line) {
-		console.log(error + "\n" + url + "\n" + line)
-		console.log("sup")
-	}
+function createDiv(e){
+	var div = document.createElement("div");
+	div.style.width = "400px";
+	div.style.height = "100px";
+	div.style.background = "red";
+	div.style.color = "white";
+	div.innerHTML = '<h3>' + e.detail.title + '</h3>' + e.detail.message + '<br>' + e.detail.filename + '<br>Line: ' + e.detail.lineNumber;
+	//document.body.appendChild(div);
+	//want to prepend instead of append... http://callmenick.com/post/prepend-child-javascript
+	var body = document.querySelector("body");
+	body.insertBefore(div, body.firstChild);
 }
-console.log("done injecting")
+//not magic. this code is injected and run in the user's webpage
+//http://stackoverflow.com/questions/20323600/how-to-get-errors-stack-trace-in-chrome-extension-content-script
+//codeToInject definition:
+function codeToInject() {
+	//listen for an error
+    window.addEventListener('error', function(e) {
+    	//build an error object
+    	console.log(e)
+        var error = {
+        	title: (e.error.stack).substring(0, e.error.stack.indexOf(':')),
+        	message: e.error.message,
+            stack: e.error.stack,
+            filename: e.filename,
+            lineNumber: e.lineno
+            // Add here any other properties you need, like e.filename, etc...
+        };
+        //dispatch a message to a listener in the content script
+        document.dispatchEvent(new CustomEvent('ReportError', {detail:error}));
+    });
+}
 
-
-
-var t = document.createTextNode("Hello World");
-document.body.appendChild(t);
-
-//at document_idle, the codeToInject function will be injected to run in webpage context
+//Inject code...
 //create script element
-var s = document.createElement('script');
-//set the source to where codeToInject is defined
-s.src = chrome.extension.getURL('error_reporter.js');
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
+var script = document.createElement('script');
+//specify function
+script.textContent = '(' + codeToInject + '())';
 //add the script to the user's webpage
-(document.head||document.documentElement).appendChild(s);
-
-
+(document.head||document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
 
 
 //listen for a message from the injected code
 document.addEventListener('ReportError', function(e) {
 	//retrieve error
-    console.log('CONTENT SCRIPT', e.detail.stack);
+    console.log('CONTENT SCRIPT', e.detail.stack, e.detail.filename, e.detail.lineNumber);
+    //format the error and insert into page
+    createDiv(e)
+    //alert(e)
 });
 
