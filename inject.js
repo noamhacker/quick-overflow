7c8ab638dfc181162f9dbc27243b797ede2f906f
Noam <13nah@queensu.ca>
2016-03-01 23:07:53 -0500

    implement popup with checkbox to enable/disable the error messages. switch from localStorage to chrome.storage (since content script needs to access storage too) - convert code to be asynchronous-friendly

--- inject.js
+++ inject.js
@@ -1,15 +1,48 @@
+var is_enabled = ""
+
+inject_listen_insert();
+
+function inject_listen_insert()
+{
+	//Inject code...
+	//create script element
+	var script = document.createElement('script');
+	//specify function
+	script.textContent = '(' + codeToInject + '())';
+	//add the script to the user's webpage
+	(document.head||document.documentElement).appendChild(script);
+	script.parentNode.removeChild(script);
+
+	//listen for a message from the injected code
+	document.addEventListener('ReportError', function(e) {
+		//retrieve error
+	    console.log('CONTENT SCRIPT', e.detail.stack, e.detail.filename, e.detail.lineNumber);
+	    //format the error and insert into page - ONLY if extension is enabled
+	    chrome.storage.local.get('enabled', function(result){
+			is_enabled = result.enabled;
+			if (is_enabled == 'true')
+			{
+				createDiv(e)
+			}
+		});
+	    //alert(e)
+	});
+}
+
 function createDiv(e){
 	var div = document.createElement("div");
-	div.style.width = "400px";
+	div.style.width = "100%";
 	div.style.height = "100px";
 	div.style.background = "red";
 	div.style.color = "white";
+	//div.style.z-index = 
 	div.innerHTML = '<h3>' + e.detail.title + '</h3>' + e.detail.message + '<br>' + e.detail.filename + '<br>Line: ' + e.detail.lineNumber;
 	//document.body.appendChild(div);
 	//want to prepend instead of append... http://callmenick.com/post/prepend-child-javascript
 	var body = document.querySelector("body");
 	body.insertBefore(div, body.firstChild);
 }
+
 //not magic. this code is injected and run in the user's webpage
 //http://stackoverflow.com/questions/20323600/how-to-get-errors-stack-trace-in-chrome-extension-content-script
 //codeToInject definition:
@@ -30,23 +63,3 @@ function codeToInject() {
         document.dispatchEvent(new CustomEvent('ReportError', {detail:error}));
     });
 }
-
-//Inject code...
-//create script element
-var script = document.createElement('script');
-//specify function
-script.textContent = '(' + codeToInject + '())';
-//add the script to the user's webpage
-(document.head||document.documentElement).appendChild(script);
-script.parentNode.removeChild(script);
-
-
-//listen for a message from the injected code
-document.addEventListener('ReportError', function(e) {
-	//retrieve error
-    console.log('CONTENT SCRIPT', e.detail.stack, e.detail.filename, e.detail.lineNumber);
-    //format the error and insert into page
-    createDiv(e)
-    //alert(e)
-});
-
--- popup.js
+++ popup.js
@@ -1,48 +1,55 @@
+
 initialize();
+listen();
 
 function initialize() {
 	//get the state of the extension... 
 	//http://stackoverflow.com/questions/2153070/do-chrome-extensions-have-access-to-local-storage
-	var isEnabled = localStorage["enabled"];
-
-	//when first installed, isEnabled won't have a value, so set it to true
-	if (typeof isEnabled === 'undefined')
-	{
-		localStorage["enabled"] = true;
-		isEnabled = localStorage["enabled"];
-	}
-
-	document.getElementById("test").innerHTML = isEnabled
-
-	//ensure checkbox matches what the local storage says
-	if (localStorage["enabled"] == true){
-		document.getElementById("state").checked = true;
-	}
-	else {
-		document.getElementById("state").checked = false;
-		//document.getElementById("test").innerHTML = "what"
-	}
-
+	var isEnabled = "";
+	chrome.storage.local.get('enabled', function(result){
+		//alert(result);
+		set_checkbox(result.enabled);
+	});
 }
 
+function set_checkbox(isEnabled)
+{
+	//ensure checkbox matches what the local storage says
+	if (isEnabled == 'true') {
+		document.getElementById("state").checked = true;
+	}
+	else if (isEnabled == 'false'){
+		document.getElementById("state").checked = false;
+	}
+	//when first installed, isEnabled won't have a value, so set it to true
+	else if (typeof isEnabled === 'undefined')
+	{
+		isEnabled = true;
+		chrome.storage.local.set({'enabled': 'true'}, function(){
+			set_checkbox('true');
+		});
+	}
+	listen();
+}
 
+function listen()
+{
+	//thanks http://stackoverflow.com/a/27096908/4926817
+	document.addEventListener("DOMContentLoaded", function (event) {
+	    var _selector = document.querySelector('input[id="state"]');
+	    _selector.addEventListener('change', function (event) {
+	        if (_selector.checked) {
+	            //store the state
+				chrome.storage.local.set({'enabled': 'true'}, function(){
+					set_checkbox('true');
+				});
+	        } else {
+	            //store the state
+	     		chrome.storage.local.set({'enabled': 'false'}, function(){
+	     			set_checkbox('false');
+	     		});
+	        }
+	    });
+	});
+}
 
-
-//thanks http://stackoverflow.com/a/27096908/4926817
-document.addEventListener("DOMContentLoaded", function (event) {
-    var _selector = document.querySelector('input[id="state"]');
-    _selector.addEventListener('change', function (event) {
-        if (_selector.checked) {
-            // do something if checked
-            document.getElementById("test").innerHTML = "on"
-            document.getElementById("state").checked = true;
-            //store the state
-            localStorage["enabled"] = true;
-        } else {
-            document.getElementById("test").innerHTML = "off"
-            document.getElementById("state").checked = false;
-            //store the state
-            localStorage["enabled"] = false;
-        }
-    });
-});
