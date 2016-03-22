24fc03699c6e60d7c82638ba79fc70b9afdc4834
Noam <13nah@queensu.ca>
2016-03-21 23:06:34 -0400

    improve appearance, add file reading to print out line causing error, improve test html page

--- inject.js
+++ inject.js
@@ -31,15 +31,38 @@ function inject_listen_insert()
 
 function createDiv(e){
 	var div = document.createElement("div");
-	div.style.width = "100%";
-	//div.style.height = "100px";
-	div.style.background = "orange";
-	div.style.color = "white";
+
+	div.setAttribute('style',  'box-shadow: 10px 10px 40px #888888; \
+								z-index: 99; \
+								background-color: #ffb734; \
+								border-radius: 25px; \
+								padding: 20px; \
+								margin-bottom: 20px; \
+								');
+	//div.style.width = "100%";
+	div.style.fontFamily = 'Arial Bold,Arial,sans-serif'; 
+
 	//div.style.z-index = 
 	// console.log("error")
 	// console.log(e)
 	var stack = e.detail.stack;
-	var cause = ""
+	var badCode = "";
+	if(e.detail.filename.indexOf("file://") == 0){
+			var fullFile = readTextFile(e.detail.filename);
+			if (fullFile == "00000") //returned from readTextFile if error
+				badCode = "Error reading source code file."
+			else {
+				//split into individual lines
+				var lines = fullFile.split('\n');
+				badCode = lines[e.detail.lineNumber - 1]
+				// alert (badCode)
+			}
+	}
+	else
+	{
+		badCode = "Source code file not accessible."
+	}
+	var cause = "";
 	if (!(typeof stack === 'undefined'))
 	{
 		cause = stack.substring(stack.indexOf(e.detail.message) + e.detail.message.length);
@@ -48,19 +71,28 @@ function createDiv(e){
 			cause = cause.substring(0, cause.indexOf('(file://') - 1)
 		}
 	}
-	var url = "http://stackoverflow.com/search?q=" + e.detail.title + ' ' + e.detail.message;
+	var url = "http://stackoverflow.com/search?q=Uncaught " + e.detail.title + ' ' + e.detail.message;
 	url = url.split(' ').join('+');
-	url += '+[javascript][html]';
-	div.innerHTML = '<a href="' + url + '" target="_blank"><h3>' 
-			+ e.detail.title + '</h3>' 
+	url += '+[javascript]';
+	var fontString_monospace = "'Courier New', Courier, monospace;"
+	div.innerHTML = '<a href="' + url + '" target="_blank"><q_o_title>Uncaught ' 
+			+ e.detail.title + '</q_o_title><br>' 
 			+ e.detail.message + '</a><br>' 
-			+ cause + '<br><br>'
+			+ "Error caused by: <br><br>"
+			+ '<span id="bad_code_here" style="font-family:' + fontString_monospace 
+						+ ' color:red; font-size:18px; border:1px solid red; border-radius:10px; padding:10px;"></span><br><br>'
+			+ cause + "<br>"
 			+ e.detail.filename + '<br>Line: ' 
 			+ e.detail.lineNumber;
-	//document.body.appendChild(div);
+	
+	//document.body.appendChild(div); - this is not what we want
 	//want to prepend instead of append... http://callmenick.com/post/prepend-child-javascript
 	var body = document.querySelector("body");
 	body.insertBefore(div, body.firstChild);
+
+	//we don't want to render the html that is in the bad code, just the text, so we must use textContent
+	document.getElementById("bad_code_here").textContent = badCode;
+	
 }
 
 
@@ -96,4 +128,31 @@ function codeToInject() {
         //dispatch a message to a listener in the content script
         document.dispatchEvent(new CustomEvent('ReportError', {detail:error}));
     });
+}
+
+
+//thanks http://stackoverflow.com/questions/14446447/javascript-read-local-text-file
+function readTextFile(file)
+{
+	var allText = "";
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                allText = rawFile.responseText;
+                // alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+    if (allText != "")
+    {
+    	return allText;
+    }
+    else
+    	return "00000"
 }
